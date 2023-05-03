8f6bfdf9f2f8fb8502103116c329048809bfd4bf
Jon Gauer <jongauer@Jons-MBP.hsd1.co.comcast.net>
2023-05-03 13:08:34 -0600

    Changed deck to show face cards, have 4 of each card, and be depleted when each card is dealt.

--- blackjack.py
+++ blackjack.py
@@ -1,24 +1,45 @@
-import random
+# Features to think about adding:
+# Make a betting pot
+# Make multiplayer possible (with player names)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+import os
+
+cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "Ace", "Ace", "Ace", "Ace", "Jack", "Jack", "Jack", "Jack", "Queen", "Queen", "Queen", "Queen", "King", "King", "King", "King"]
 
 def play_blackjack():
-
+    os.system('clear')
+    print("\nWelcome to Blackjack. Aces are both high and low.")
     def deal_card():
-        return random.choice(cards)
+        chosen_card = random.choice(cards)
+        cards.remove(chosen_card)
+        return chosen_card
+    
+    def convert_face_cards(card_list):
+        numeric_cards = card_list[:]
+        for card in numeric_cards:
+            if card in ['Jack', 'Queen', 'King']:
+                numeric_cards.remove(card)
+                numeric_cards.append(10)
+            elif card == 'Ace':
+                numeric_cards.remove(card)
+                numeric_cards.append(11)
+        return numeric_cards
+
 
     def calculate_score(card_list):
-        hand_total = sum(card_list)
+        numeric_cards = convert_face_cards(card_list)
+        hand_total = sum(numeric_cards)
         if hand_total == 21:
             return 0
-        elif (hand_total > 21) and (11 in card_list):
-            i = card_list.index(11)
-            card_list[i] = 1
-            hand_total = sum(card_list)
+        elif (hand_total > 21) and (11 in numeric_cards):
+            numeric_cards.remove(11)
+            numeric_cards.append(1)
+            hand_total = sum(numeric_cards)
         return hand_total
     
     def display_score():
-        print(f"\nPlayer's hand is {user_cards}. Computer has {len(computer_cards)} cards, one of which is {computer_cards[0]}.")
+        print(f"\nPlayer's hand is {user_cards}. Computer has {len(computer_cards)} cards, one of which is a {computer_cards[0]}.")
 
     def play_again():
         answer = input("\nWould you like to play again? Y or N: ").lower()
@@ -32,13 +53,14 @@ def play_blackjack():
             play_again()
 
     def check_for_blackjack():
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
+        user_score = calculate_score(convert_face_cards(user_cards))
+        computer_score = calculate_score(convert_face_cards(computer_cards))
         if user_score > 21 and computer_score > 21:
             print("\nYou and the computer both went over 21. Draw.")
             play_again()
         elif user_score > 21:
             print(f"\nYou went over 21. Computer had {computer_score}. You lose.")
+            play_again()
         elif computer_score > 21:
             print(f"\nComputer went over 21. You had {user_score}. You win.")
             play_again()
@@ -46,8 +68,8 @@ def play_blackjack():
             return False
     
     def compare_scores():
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
+        user_score = calculate_score(convert_face_cards(user_cards))
+        computer_score = calculate_score(convert_face_cards(computer_cards))
         if user_score < computer_score:
             print(f"\nYou had {user_score} while computer had {computer_score}. You lose.")
             play_again()
@@ -69,6 +91,7 @@ def play_blackjack():
 
     while check_for_blackjack() == False:
         hit_or_not = input("\nDo you want to draw another card? Y or N: ").lower()
+        os.system('clear')
         if hit_or_not == 'y':
             new_card = deal_card()
             print(f"\nDealing you a new card...\n\nYour new card is: {new_card}")
